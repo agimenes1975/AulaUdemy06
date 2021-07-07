ded9347170f2a30be5e0414c148f19022bc40e51
agimenes1975 <angelogimenes@yahoo.com.br>
2021-07-07 20:45:19 -0300

    Criado todos os pacotes e classes

---
+++ src/entities/Departament.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Departament {
+	
+	private String name;
+	
+	public Departament() {	//Construtor Vazio	
+	}
+
+	public Departament(String name) { //Construtor com parametros		
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+
+}
